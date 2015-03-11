47ad7b657337e1f4a1270d1e8e8cc4eeeacdc837
MightyWaffles <piperlikespie@gmail.com>
2015-03-10 17:04:15 -0700

    Major Code Fix
    fixed code, works now
    
    -turns on* when 12 gets power (can be supplied by 11)
    
    *pin 13

---
+++ Spider_Eyes_XV_Arduino.ino
@@ -0,0 +1,33 @@
+
+
+int LEDPin, SwitchOutPin, SwitchInPin;
+//all pins are now in one place
+
+void setup() {
+// put your setup code here, to run once
+  LEDPin = 13;
+  //LED pin chain
+  SwitchInPin = 12;
+  //input PVsensor
+  SwitchOutPin = 11;
+  //output PVSensor
+  
+  pinMode(SwitchInPin, INPUT);
+  pinMode(SwitchOutPin, OUTPUT);
+  pinMode(LEDPin, OUTPUT);
+  
+  digitalWrite(SwitchOutPin, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(SwitchInPin) == HIGH) 
+  {
+    digitalWrite(LEDPin, HIGH);
+  } 
+  else
+  {
+    digitalWrite(13, LOW);
+  }
+  delay(1000);
+}
