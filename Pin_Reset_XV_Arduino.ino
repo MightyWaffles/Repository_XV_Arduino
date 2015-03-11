b485c8b7841b748af294a9b15d4f581a584b52e3
MightyWaffles <piperlikespie@gmail.com>
2015-03-10 17:06:04 -0700

    This is for express turn off.
    run this to turn all pins on low (so far)

---
+++ Pin_Reset_XV_Arduino.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  digitalWrite(13, LOW);
+  //turn off LED chain
+  digitalWrite(11, LOW);
+  //turn off Button power
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+//this code should be updated when a new pin is put in use
