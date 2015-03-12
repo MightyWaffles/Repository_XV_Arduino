d989de6339cb11ee8e3cf279d3a983cfb2048f38
MightyWaffles <piperlikespie@gmail.com>
2015-03-11 17:05:06 -0700

    Update Pin_Reset_XV_Arduino.ino
    simplification.
    
    THIS IS DONE

--- Pin_Reset_XV_Arduino.ino
+++ Pin_Reset_XV_Arduino.ino
@@ -1,14 +1,2 @@
-void setup() {
-  // put your setup code here, to run once:
-  digitalWrite(13, LOW);
-  //turn off LED chain
-  digitalWrite(11, LOW);
-  //turn off Button power
-  
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-}
-//this code should be updated when a new pin is put in use
+void setup() {}
+void loop() {}
